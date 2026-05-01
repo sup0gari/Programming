2dedaa307c925f7801717b9b4da8dc7499f53a11
sup0gari <sup0garixlolz@gmail.com>
2026-05-01 19:39:13 +0900

    autoNmap modified

--- Others/autoNmap.sh
+++ Others/autoNmap.sh
@@ -7,11 +7,33 @@ if [ -z "$IP" ]; then
     exit 1
 fi
 
-PORTS = $(nmap -p- -Pn --min-rate=5000 $IP | grep '^[0-9]' | cut -d '/' -f1 | tr '\n' ',' | sed 's/,$//')
+spinner() {
+    local pid=$1
+    local delay=0.1
+    local spinstr='/-\|'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+
+echo "[*] Phase 1: Scanning all ports on $IP (High Speed)..."
+
+(nmap -p- -Pn --min-rate=5000 $IP | grep '^[0-9]' | cut -d '/' -f1 | tr '\n' ',' | sed 's/,$//' > /tmp/ports.txt) &
+
+spinner $!
+
+PORTS=$(cat /tmp/ports.txt)
 
 if [ -z "$PORTS" ]; then
-    echo "No scannable ports found."
+    echo "[!] No scannable ports found."
     exit 1
 fi
+
+spinner $!
 
 nmap -p $PORTS -v -sCV -Pn --min-rate=5000 $IP
