03c1289dd31117522dd9477fa43998d427fe18f1
sup0gari <sup0garixlolz@gmail.com>
2026-05-01 19:35:02 +0900

    autoNmap modified

--- Others/autoNmap.sh
+++ Others/autoNmap.sh
@@ -2,14 +2,14 @@
 
 IP=$1
 
-if [-z "$IP"]; then
+if [ -z "$IP" ]; then
     echo "Usage: $0 <IP>"
     exit 1
 fi
 
-PORTS = $(nmap -p- -Pn --min-rate=5000 $IP | grep '^[0-9]' | cut -d'/'-f1 | tr '\n' ',' | sed 's/,$//')
+PORTS = $(nmap -p- -Pn --min-rate=5000 $IP | grep '^[0-9]' | cut -d '/' -f1 | tr '\n' ',' | sed 's/,$//')
 
-if [-z "$PORTS"]; then
+if [ -z "$PORTS" ]; then
     echo "No scannable ports found."
     exit 1
 fi
