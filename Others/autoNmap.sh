3de4d94f1fb711ca34dd3fce39d21fcce9def3b7
sup0gari <sup0garixlolz@gmail.com>
2026-05-25 08:55:29 +0900

    autoNmap modified

--- Others/autoNmap.sh
+++ Others/autoNmap.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-IP=$1
-
-if [ -z "$IP" ]; then
+if [ -z "$1" ]; then
     echo "Usage: $0 <IP>"
     exit 1
 fi
+
+IP=$1
+
+echo "Select scan type for $IP:"
+echo "1) TCP only"
+echo "2) UDP only"
+echo "3) Both (TCP and UDP)"
+read -p "Choice [1-3]: " choice
 
 spinner() {
     local pid=$1
@@ -21,36 +27,41 @@ spinner() {
     printf "    \b\b\b\b"
 }
 
-echo "[*] Scanning TCP & UDP ports..."
+scan_tcp() {
+    echo "[*] Scanning TCP ports..."
+    (nmap -p- -Pn --min-rate=5000 $IP | grep '^[0-9]' | cut -d '/' -f 1 | tr '\n' ',' | sed 's/,$//' > /tmp/ports_tcp.txt) &
+    PID_TCP=$!
+    echo -n "Scanning TCP..."
+    spinner $PID_TCP
+    echo "Done."
+    TCP_PORTS=$(cat /tmp/ports_tcp.txt)
+    if [ -n "$TCP_PORTS" ]; then
+        echo -e "\n[+] TCP Service Enumeration (Ports: $TCP_PORTS)"
+        nmap -p $TCP_PORTS -sCV -Pn --min-rate=5000 $IP
+    else
+        echo "[!] No TCP ports found."
+    fi
+}
 
-(nmap -p- -Pn --min-rate=5000 $IP | grep '^[0-9]' | cut -d '/' -f 1 | tr '\n' ',' | sed 's/,$//' > /tmp/ports_tcp.txt) &
-PID_TCP=$!
+scan_udp() {
+    echo "[*] Scanning UDP ports..."
+    (nmap -sU --top-ports 100 -Pn --min-rate=5000 $IP | grep '^[0-9]' | cut -d '/' -f 1 | tr '\n' ',' | sed 's/,$//' > /tmp/ports_udp.txt) &
+    PID_UDP=$!
+    echo -n "Scanning UDP..."
+    spinner $PID_UDP
+    echo "Done."
+    UDP_PORTS=$(cat /tmp/ports_udp.txt)
+    if [ -n "$UDP_PORTS" ]; then
+        echo -e "\n[+] UDP Service Enumeration (Ports: $UDP_PORTS)"
+        nmap -p $UDP_PORTS -sUV -Pn --min-rate=5000 $IP
+    else
+        echo "[!] No UDP ports found."
+    fi
+}
 
-(nmap -sU --top-ports 100 -Pn --min-rate=5000 $IP | grep '^[0-9]' | cut -d '/' -f 1 | tr '\n' ',' | sed 's/,$//' > /tmp/ports_udp.txt) &
-PID_UDP=$!
-
-echo -n "Scanning TCP..."
-spinner $PID_TCP
-echo "Done."
-
-echo -n "Scanning UDP..."
-spinner $PID_UDP
-echo "Done."
-
-TCP_PORTS=$(cat /tmp/ports_tcp.txt)
-UDP_PORTS=$(cat /tmp/ports_udp.txt)
-
-if [ -n "$TCP_PORTS" ]; then
-    echo -e "\n[+] TCP Service Enumeration (Ports: $TCP_PORTS)"
-    nmap -p $TCP_PORTS -sCV -Pn --min-rate=5000 $IP
-fi
-
-if [ -n "$UDP_PORTS" ]; then
-    echo -e "\n[+] UDP Service Enumeration (Ports: $UDP_PORTS)"
-    nmap -p $UDP_PORTS -sUV -Pn --min-rate=5000 $IP
-fi
-
-if [ -z "$TCP_PORTS" ] && [ -z "$UDP_PORTS" ]; then
-    echo "[!] No scannable ports found."
-    exit 1
-fi
+case $choice in
+    1) scan_tcp ;;
+    2) scan_udp ;;
+    3) scan_tcp; scan_udp ;;
+    *) echo "Invalid choice"; exit 1 ;;
+esac
